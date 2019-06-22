f9df9f5e5f7d16ea9dcfc9b489ce2d557f8727c0
Steve Teece <sgteece@hotmail.com>
2019-06-22 10:36:10 +1000

    create timer conttol

--- Timer/ViewModels/HomeViewModel.cs
+++ Timer/ViewModels/HomeViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
-using Windows.Foundation;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 
 
@@ -10,72 +7,25 @@ namespace Timer.ViewModels
 {
     public class HomeViewModel : Screen
     {
-        TimeSpan oneSecond = TimeSpan.Parse("0:0:01");
-        DispatcherTimer countdownTimer = new DispatcherTimer();
-        TimeSpan _timeRemaining;
 
         public HomeViewModel()
         {
             
-            TimeRemaining = TimeSpan.Parse("2:0:0");
-
-            countdownTimer.Interval = oneSecond;
-            countdownTimer.Tick += CountdownTimer_Tick;
-
-            ApplicationView.PreferredLaunchViewSize = new Size(300, 200);
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(300, 200));
 
         }
 
-        private void CountdownTimer_Tick(object sender, object e)
-        {
-            TimeRemaining -= oneSecond;
-            
-            if (TimeRemaining < oneSecond)
-            {
-                countdownTimer.Stop();
-            }
-        }
 
-        public TimeSpan TimeRemaining
-        {
-            get { return _timeRemaining; }
-            set
-            {
-                _timeRemaining = value;
-                NotifyOfPropertyChange(() => TimeRemaining);
-            }
-        }
+        //private void SettingsButton(object sender, RoutedEventArgs e)
+        //{
+        //    throw new NotImplementedException();
 
-            private void SettingsButton(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+        //}
 
-        }
-
-        public List<string> Activities { get; set; }
+        // public List<string> Activities { get; set; }
 
 
 
-        public void StartButton()
-        {
-            
-            if (countdownTimer.IsEnabled == false)
-            {
-                // The countdown timer is not running
-                countdownTimer.Start();
 
-                // TODO: Change the text on the button to read "Stop Timer"
-                
-            }
-            else
-            {
-                countdownTimer.Stop();
 
-                // TODO: Change the text on the button to read "Start Timer"
-            }
-
-        }
     }
 }
--- Timer/ViewModels/TimerViewModel.cs
+++ Timer/ViewModels/TimerViewModel.cs
@@ -3,11 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Caliburn.Micro;
+using Windows.Foundation;
 
 namespace Timer.ViewModels
 {
     // TODO: Move timer code to new viewmodel
-    class TimerViewModel
+    public class TimerViewModel : Screen
     {
+        TimeSpan oneSecond = TimeSpan.Parse("0:0:01");
+        DispatcherTimer countdownTimer = new DispatcherTimer();
+        TimeSpan _timeRemaining;
+
+        public TimerViewModel()
+        {
+            TimeRemaining = TimeSpan.Parse("2:0:0");
+
+            countdownTimer.Interval = oneSecond;
+            countdownTimer.Tick += CountdownTimer_Tick;
+
+            ApplicationView.PreferredLaunchViewSize = new Size(300, 200);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(300, 200));
+
+        }
+
+        private void CountdownTimer_Tick(object sender, object e)
+        {
+            TimeRemaining -= oneSecond;
+
+            if (TimeRemaining < oneSecond)
+            {
+                countdownTimer.Stop();
+            }
+        }
+
+        public TimeSpan TimeRemaining
+        {
+            get { return _timeRemaining; }
+            set
+            {
+                _timeRemaining = value;
+                NotifyOfPropertyChange(() => TimeRemaining);
+            }
+        }
+
+        public void StartButton()
+        {
+
+            if (countdownTimer.IsEnabled == false)
+            {
+                // The countdown timer is not running
+                countdownTimer.Start();
+
+                // TODO: Change the text on the button to read "Stop Timer"
+
+            }
+            else
+            {
+                countdownTimer.Stop();
+
+                // TODO: Change the text on the button to read "Start Timer"
+            }
+
+        }
     }
 }
